7fee55f898d7b225b90b260eec3bf3bcc1b1bd18
annoyhacker <annoyhacker190@gmail.com>
2025-04-18 23:13:55 +0530

    Type error: Type 'User[]' is missing the following properties from type 'User': id, name, email, password

--- auth.ts
+++ auth.ts
@@ -8,14 +8,13 @@ import { sql } from '@vercel/postgres';
 
 async function getUser(email: string): Promise<User | undefined> {
     try {
-        const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
-        return user.rows[0];
+        const result = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+        return result.rows[0];
     } catch (error) {
         console.error('Failed to fetch user:', error);
         throw new Error('Failed to fetch user.');
     }
 }
-
 
 export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
@@ -23,7 +22,10 @@ export const { auth, signIn, signOut } = NextAuth({
         Credentials({
             async authorize(credentials) {
                 const parsedCredentials = z
-                    .object({ email: z.string().email(), password: z.string().min(6) })
+                    .object({
+                        email: z.string().email(),
+                        password: z.string().min(6)
+                    })
                     .safeParse(credentials);
 
                 if (parsedCredentials.success) {
