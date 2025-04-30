a7f406185951cb5cb66607915fa8e52a3f241d2a
annoyhacker <annoyhacker190@gmail.com>
2025-04-30 18:14:23 +0530

    error type error

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -5,11 +5,13 @@ import { notFound } from 'next/navigation';
 
 export const dynamic = 'force-dynamic';
 
-export default async function Page({
-    params,
-}: Readonly<{
-    params: { id: string };
-}>) {
+interface PageProps {
+    params: {
+        id: any;
+    };
+}
+
+export default async function Page({ params }: PageProps) {
     try {
         const [invoice, customers] = await Promise.all([
             fetchInvoiceById(params.id),
