a4f9980fa686382276e34e0625eaadcf1e691bfb
annoyhacker <annoyhacker190@gmail.com>
2025-04-30 18:03:39 +0530

    type error issue

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -8,7 +8,7 @@ export const dynamic = 'force-dynamic';
 export default async function Page({
     params,
 }: Readonly<{
-    params: { id: any };
+    params: { id: string };
 }>) {
     try {
         const [invoice, customers] = await Promise.all([
